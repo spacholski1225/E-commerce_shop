54ecab00e75d99516d230a3095a2dd199d8174c2
spacholski1225 <spacholski1225@gmail.com>
2021-02-16 09:15:08 +0100

    first commit

---
+++ EmpikClone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmpikClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
