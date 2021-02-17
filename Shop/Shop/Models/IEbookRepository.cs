576756e03e59e41691c82b0fe30f6222be07e8fa
spacholski1225 <spacholski1225@gmail.com>
2021-02-17 09:55:58 +0100

    started clear project, created first models

---
+++ Shop/Shop/Models/IEbookRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models
+{
+    public interface IEbookRepository
+    {
+        IEnumerable<Ebook> GetAllEbooks { get; }
+        Ebook GetEbookById(int ebookId);
+    }
+}
