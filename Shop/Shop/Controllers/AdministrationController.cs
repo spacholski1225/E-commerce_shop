5889e05dd87355d7e02824b1c5938de62d7e9db4
spacholski1225 <spacholski1225@gmail.com>
2021-02-26 16:10:47 +0100

    added possibility to create a role in current login user, in future I'm going to add creatable only for administrators

---
+++ Shop/Shop/Controllers/AdministrationController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shop.ViewModels;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+            IdentityResult identityResult = await roleManager.CreateAsync(identityRole);
+                if (identityResult.Succeeded)
+                {
+                    return RedirectToAction("list", "ebook");
+                }
+
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var role = roleManager.Roles;
+            return View(role);
+        }
+    }
+}
