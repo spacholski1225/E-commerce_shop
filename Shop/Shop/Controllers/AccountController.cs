c03db975b9d88f224e1a1f53947a7f5b96d23023
spacholski1225 <spacholski1225@gmail.com>
2021-02-27 11:25:34 +0100

    edit rols added

--- Shop/Shop/Controllers/AccountController.cs
+++ Shop/Shop/Controllers/AccountController.cs
@@ -29,7 +29,12 @@ namespace Shop.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                { 
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
                 var result = await  _userManager.CreateAsync(user, model.Password);
 
                 if (result.Succeeded)
--- Shop/Shop/Controllers/AdministrationController.cs
+++ Shop/Shop/Controllers/AdministrationController.cs
@@ -1,22 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Shop.Models.ApplicationUser;
 using Shop.ViewModels;
 using System.Threading.Tasks;
 
 namespace Shop.Controllers
 {
-    [Authorize]
+
     public class AdministrationController : Controller
     {
-        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
-            this.roleManager = roleManager;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
-       
+
         [HttpGet]
         public IActionResult CreateRole()
         {
@@ -32,7 +35,7 @@ namespace Shop.Controllers
                 {
                     Name = model.RoleName
                 };
-            IdentityResult identityResult = await roleManager.CreateAsync(identityRole);
+                IdentityResult identityResult = await _roleManager.CreateAsync(identityRole);
                 if (identityResult.Succeeded)
                 {
                     return RedirectToAction("list", "ebook");
@@ -49,8 +52,69 @@ namespace Shop.Controllers
         [HttpGet]
         public IActionResult ListRoles()
         {
-            var role = roleManager.Roles;
+            var role = _roleManager.Roles;
             return View(role);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("../Error/NotFound.cshtml");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            foreach (var user in _userManager.Users)
+            {
+
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+
+            }
+            return View(model);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                return View("../Error/NotFound.cshtml");
+            }
+
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+
+
+
+        }
+
     }
 }
