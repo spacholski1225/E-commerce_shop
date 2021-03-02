e0468ae0118ed3e71f19d085a4a0c71b7f5dfcd0
spacholski1225 <spacholski1225@gmail.com>
2021-03-02 20:41:54 +0100

    created confirm email

--- Shop/Shop/Controllers/AccountController.cs
+++ Shop/Shop/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Shop.Models.ApplicationUser;
 using Shop.ViewModels;
 using System;
@@ -15,10 +16,13 @@ namespace Shop.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ILogger<AccountController> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _logger = logger;
         }
         [HttpGet]
         public IActionResult Register()
@@ -42,6 +46,13 @@ namespace Shop.Controllers
 
                 if (result.Succeeded)
                 {
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var confirmationLink = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, token = token }, Request.Scheme);
+
+                    _logger.Log(LogLevel.Warning, confirmationLink);
+
+
+
                     var emailEnd = user.NormalizedEmail.Split('@').Last();
                     if (emailEnd == "ADMIN.COM")
                     {
@@ -52,8 +63,10 @@ namespace Shop.Controllers
                     {
                         await _userManager.AddToRoleAsync(user, "User");
                     }
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return RedirectToAction("index", "home");
+
+                    ViewBag.ErrorTitle = "Registration successful";
+                    ViewBag.ErrorMessage = "Before you can login, please confirm your email";
+                    return View("NotFound");
                 }
 
                 foreach (var error in result.Errors)
@@ -86,8 +99,19 @@ namespace Shop.Controllers
         [AllowAnonymous]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
+
+            model.ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
             if (ModelState.IsValid)
             {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user != null && !user.EmailConfirmed && (await _userManager.CheckPasswordAsync(user, model.Password)))
+                {
+                    ModelState.AddModelError(string.Empty, "Email not confirmed yet");
+                    return View(model);
+                }
+
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
 
                 if (result.Succeeded)
@@ -100,6 +124,32 @@ namespace Shop.Controllers
 
             }
             return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if(userId == null || token == null)
+            {
+                return RedirectToAction("index", "home");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = $"The User ID {userId} is invalid";
+                return View("NotFound");
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+
+            if (result.Succeeded)
+            {
+                return View();
+            }
+            ViewBag.ErrorTitle = "Email cannot be confirmed";
+            return View("NotFound");
         }
 
         [HttpGet]
@@ -149,6 +199,10 @@ namespace Shop.Controllers
                 return View("Login", loginViewModel);
             }
 
+            var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+
+            ApplicationUser user = null;
+
             var signInResult = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey,
                                                                              isPersistent: false, bypassTwoFactor: true);
 
@@ -158,12 +212,9 @@ namespace Shop.Controllers
                 return LocalRedirect(returnUrl);
             }
 
-            var email = info.Principal.FindFirstValue(ClaimTypes.Email);
 
             if(email != null)
             {
-                var user = await _userManager.FindByEmailAsync(email);
-
                 if(user == null)
                 {
                     user = new ApplicationUser
