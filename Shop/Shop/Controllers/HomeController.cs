65b87abbe96f51bb05b0468368b14644f4246752
spacholski1225 <spacholski1225@gmail.com>
2021-03-05 15:11:55 +0100

    finished contact form with message sending in to logs

--- Shop/Shop/Controllers/HomeController.cs
+++ Shop/Shop/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Shop.Models;
+using Shop.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Shop.Controllers
@@ -18,12 +20,22 @@ namespace Shop.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Contact()
         {
             return View();
         }
-        public IActionResult Contact()
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
         {
+            var from = model.Email;
+            MailMessage message = new MailMessage(from, "spacholski1225@gmail.com");
+            message.Subject = "Contact from ecommerce" + model.FirstName + ", " + model.LastName + ": " + model.Email;
+            message.Body = model.Message;
+            //there should by stmp config
+            string email = message.Subject + " | " + message.Body;
+            _logger.Log(LogLevel.Warning, email);
             return View();
         }
 
@@ -31,6 +43,6 @@ namespace Shop.Controllers
         {
             return View();
         }
-        
+
     }
 }
---
+++ Shop/Shop/ViewModels/ContactViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Message { get; set; }
+        public string Email { get; set; }
+    }
+}
