2a71c4121cc332d4a4c9424d442e4b2d8a40f147
spacholski1225 <spacholski1225@gmail.com>
2021-03-13 17:22:32 +0100

    introduction into tests

---
+++ Shop/Shop.Tests/OrderControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Shop.Tests
+{
+    public class OrderControllerTest
+    {
+        [Fact]
+        public void TestIfShoppingCartTotalInNotEmpty()
+        {
+
+        }
+    }
+}
