fab562cd49248f23d0a9696ebd64947b9a524398
spacholski1225 <spacholski1225@gmail.com>
2021-03-15 21:34:55 +0100

    fixed problem with compatibility

---
+++ Shop/Shop.Text/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using Shop.Controllers;
+
+namespace Shop.Test
+{
+    public class EbookControllerTest
+    {
+        private readonly EbookController
+        public EbookControllerTest()
+        {
+
+        }
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
