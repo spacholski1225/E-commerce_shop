5eea163354763423c2edc1817b434634c385303f
spacholski1225 <spacholski1225@gmail.com>
2021-03-16 19:03:04 +0100

    create post test

---
+++ Shop/Shop.Text/EbookControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Moq;
+using NUnit.Framework;
+using Shop.Controllers;
+using Shop.Data;
+using Shop.Models;
+using Shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Test
+{
+    [TestFixture]
+    public class EbookControllerTest
+    {
+        private readonly Mock<IEbookRepository> _ebookRepoMock;
+        private readonly Mock<IWebHostEnvironment> _hostingEnvironmentMock;
+        private readonly Mock<ShopDbContext> _contextMock;
+
+        private readonly EbookController _controller;
+        public EbookControllerTest()
+        {
+            _ebookRepoMock = new Mock<IEbookRepository>();
+            _hostingEnvironmentMock = new Mock<IWebHostEnvironment>();
+            _contextMock = new Mock<ShopDbContext>();
+
+            _controller = new EbookController(_ebookRepoMock.Object, _hostingEnvironmentMock.Object, _contextMock.Object);
+        }
+
+        [Test]
+        public void Create_CreateNew_ExpectTrue()
+        {
+            bool test = false;
+            string message = "";
+
+            var mockEbook = new CreateEbookViewModel
+            {
+                Title = "Mock Title",
+                Description = "Mock description",
+                Price = 30,
+                Category = EEbookCategory.Horror
+               
+            };
+
+           _controller.Create(mockEbook);
+
+            var result = _ebookRepoMock.Object.GetAllEbooks.ToList();
+
+            if (result.Count > 0)
+            {
+                test = true;
+                message = "";
+            }
+            else
+            {
+                test = false;
+                message = "Error ebook does not add";
+            }
+
+            Assert.IsTrue(test, message);
+        }
+    }
+}
